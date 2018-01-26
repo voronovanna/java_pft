e3fd5802d014216645b5682ea1e1980a8bdff4b7
voronovanna <voronova.anka@gmail.com>
2018-01-26 02:22:31 +0200

    задание 2.0

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,13 +5,20 @@ public class MyFirstProgram {
         hello("user");
 
         Square s = new Square(7);
-        System.out.println("Площадь квадрата = "+s.area());
+        System.out.println("Площадь квадрата = " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
-        System.out.println("Площадь прямоугольника="+r.area());
+        System.out.println("Площадь прямоугольника=" + r.area());
+        
+        double p1 = 9;
+        double p2 = 12;
+        System.out.println("Pасстояние между двумя точками = " + Math.sqrt((p1*p1)+(p2*p2)));
     }
 
-    public static void hello (String somebody){
-        System.out.println("Hello,"+somebody);
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody);
     }
+
+    //public static double distance(Point p1, Point p2){
+    //}
 }
