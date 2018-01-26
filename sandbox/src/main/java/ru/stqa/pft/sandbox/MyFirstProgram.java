17dfc764ce442a4a1bcdee532dff3f8cb4600afe
voronovanna <voronova.anka@gmail.com>
2018-01-26 02:57:30 +0200

    revert changes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,18 +9,12 @@ public class MyFirstProgram {
 
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь прямоугольника=" + r.area());
-
-        dictance (1, 10, 3, 15);
-
+        
     }
 
 
     public static void hello(String somebody) {
         System.out.println("Hello," + somebody);
-    }
-
-    public static void dictance(double x1, double x2, double y1, double y2){
-        System.out.println("Pасстояние между двумя точками = " + Math.sqrt((Math.pow(x2-x1,2)+Math.pow(y2-y1,2))));
     }
 
 }
