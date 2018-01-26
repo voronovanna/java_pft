55783812b3df9c748bd981d1314615512f64c929
voronovanna <voronova.anka@gmail.com>
2018-01-26 02:45:24 +0200

    revert changes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,16 +9,11 @@ public class MyFirstProgram {
 
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь прямоугольника=" + r.area());
-        
-        double p1 = 9;
-        double p2 = 12;
-        System.out.println("Pасстояние между двумя точками = " + Math.sqrt((p1*p1)+(p2*p2)));
+
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello," + somebody);
     }
 
-    //public static double distance(Point p1, Point p2){
-    //}
 }
