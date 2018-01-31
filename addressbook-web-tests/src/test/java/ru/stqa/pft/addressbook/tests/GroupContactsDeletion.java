07ef18fca07f7e275623e30f47a897432e0331d7
voronovanna <voronova.anka@gmail.com>
2018-01-31 02:51:51 +0200

    Задание № 6 (add class)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupContactsDeletion.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupContactsDeletion extends TestBase{
+  @Test
+  public void testContactsDeletion () {
+
+  }
+
+  }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupContactsModification.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupContactsModification extends TestBase{
+  @Test
+  public void testContactsModification () {
+
+  }
+}
