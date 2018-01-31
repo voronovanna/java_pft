c2d731df782001936351c1519e80937cb7ddecbc
voronovanna <voronova.anka@gmail.com>
2018-01-31 02:37:56 +0200

    changes 2.9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -59,4 +59,11 @@ public class GroupHelper extends HelperBase {
       field(By.name("notes"), groupDataContacts.getNotes());
     }
 
+  public void initGroupModification() {
+    click(By.name("edit"));
   }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
+  }
+}
