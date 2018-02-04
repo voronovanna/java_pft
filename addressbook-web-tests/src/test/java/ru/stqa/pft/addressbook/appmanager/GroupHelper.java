962d771c165bcde04842a6154ce71071155240fb
voronovanna <voronova.anka@gmail.com>
2018-02-04 06:35:47 +0200

    4.3 Количество элементов в коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -126,4 +126,8 @@ public class GroupHelper extends HelperBase {
   public boolean isThereAContact() {
     return isElementPresent(By.name("selected[]"));
   }
+
+  public int getGroupCount() {
+    return wd.findElements(By.name("selected[]")).size();
+  }
 }
