d0cf20a594673c5f553dbb7eafe15664dd5bf35f
voronovanna <voronova.anka@gmail.com>
2018-02-04 07:43:45 +0200

    4.6 Сравнение коллекций

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupData {
   private final String name;
   private final String header;
@@ -9,6 +11,19 @@ public class GroupData {
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
   }
 
   public String getName() {
@@ -21,5 +36,12 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            '}';
   }
 }
