4ca99859ec35b4568b87e3a7e03c7b78988bc100
voronovanna <voronova.anka@gmail.com>
2018-02-05 02:11:10 +0200

    4.10 Сортировка списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -16,8 +16,22 @@ public class GroupData {
     return id;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(name);
+  }
+
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -50,18 +64,4 @@ public class GroupData {
             '}';
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-
-    return Objects.hash(id, name);
-  }
 }
