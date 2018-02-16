eecc7ee0322549a760af2cf196cace356d3727fb
voronovanna <voronova.anka@gmail.com>
2018-02-16 22:33:11 +0200

    changes 4.10 (part 1)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupContactsModification.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupContactsModification.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDataContacts;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -27,6 +28,9 @@ public class GroupContactsModification extends TestBase{
 
     before.remove(before.size() - 1);
     before.add(contact);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super GroupDataContacts> byId = (g1, g2)->Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
     }
 }
