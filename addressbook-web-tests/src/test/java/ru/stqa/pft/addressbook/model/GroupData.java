17070e621acffb1848abb2e7c4118015b0fe1cb1
voronovanna <voronova.anka@gmail.com>
2018-02-22 16:33:07 +0200

    5.8 Хеширование и предварительные проверки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -47,19 +47,6 @@ public class GroupData {
 
     return Objects.hash(id, name);
   }
-  //  public GroupData(String name, String header, String footer) {
-//    this.id = Integer.MAX_VALUE;
-//    this.name = name;
-//    this.header = header;
-//    this.footer = footer;
-//  }
-//
-//  public GroupData(int id, String name, String header, String footer) {
-//    this.id = id;
-//    this.name = name;
-//    this.header = header;
-//    this.footer = footer;
-//  }
 
   public String getName() {
     return name;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -29,9 +29,10 @@ public class GroupDeletionTests extends TestBase {
     Groups before = app.group().all();
     GroupData deletedGroup = before.iterator().next();
     app.group().delete(deletedGroup);
+    assertThat(app.group().count(), equalTo(before.size() - 1));
     Groups after = app.group().all();
     assertEquals(after.size(), before.size() -1);
-    assertThat(after, equalTo(before.without(deletedGroup)));
+//  assertThat(after, equalTo(before.without(deletedGroup)));
   }
 
 }
