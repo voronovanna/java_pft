f6784085d5bec30296e87fbb6dbfc31b79522f45
voronovanna <voronova.anka@gmail.com>
2018-03-01 02:25:50 +0200

    Задание № 16 (part 1)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactToGroup.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AddContactToGroup extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions(){
+
+  }
+
+  @Test
+  public void testAddContactToGroup (){
+
+  }
+
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddGroupToContact.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AddGroupToContact extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions(){
+
+  }
+
+  @Test
+  public void testAddGroupToContact (){
+
+  }
+
+}
