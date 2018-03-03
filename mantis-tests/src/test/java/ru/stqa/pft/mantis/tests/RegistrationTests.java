74c9538d1767e7ff8c00e149ad1cfeba4e0c65dc
voronovanna <voronova.anka@gmail.com>
2018-03-03 04:27:36 +0200

    8.7. Получение почты на внешний почтовый сервер. Создание пользователей на внешнем почтовом сервере (по протоколу Telnet)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
 
+import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
 
@@ -19,13 +20,15 @@ public class RegistrationTests extends TestBase{
   }
 
   @Test
-  public void testRegistration() throws InterruptedException, IOException {
+  public void testRegistration() throws IOException, InterruptedException {
     long now = System.currentTimeMillis();
     String user =  String.format("user%s", now);
     String password = "password";
     String email = String.format("user%s@localhost.localdomain", now);
+//  app.james().createUser(user, password);
     app.registration().start(user, email);
     List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+//  List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user, password));
